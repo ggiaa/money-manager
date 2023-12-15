e015ef5bf714b4ebe43e347a98d67bcc00168b1d
ggiaa <giaa601@gmail.com>
2023-12-16 06:58:51 +0700

    Expense Layout

--- app/components/settings/Expense.jsx
+++ app/components/settings/Expense.jsx
@@ -1,30 +1,60 @@
-import { useBoundedStore } from "@/app/store/boundedStore";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useStore } from "zustand";
-import { PiSunglasses } from "react-icons/pi";
+import { useBoundedStore } from "@/app/store/boundedStore";
+import DinamicIcon from "@/app/utils/DinamicIcon";
+import { PiPlusCircleBold } from "react-icons/pi";
+import AddEditIncomeExpense from "./AddEditIncomeExpense";
 
 function Expense() {
   const boundedStore = useStore(useBoundedStore);
-
   const expenseCategories = useBoundedStore((state) => state.expenseCategories);
 
-  // console.log(expenseCategories);
+  const [showModal, setShowModal] = useState(false);
+  const [expense, setExpense] = useState("");
+
+  const handleAdd = () => {};
+
+  const handleClick = (expense) => {};
 
   useEffect(() => {
     boundedStore.getCategories();
   }, []);
 
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {expenseCategories.map((expense) => (
-        <div className="bg-white flex items-center p-3 rounded-lg">
-          <div>
-            <PiSunglasses className="text-5xl aspect-square bg-sky-300 rounded-full shadow-lg p-2 mr-4" />
+    <>
+      <div className="mb-3 flex justify-end">
+        <button
+          onClick={handleAdd}
+          className="bg-sky-500 px-4 py-2 text-white rounded-lg flex items-center shadow-md hover:bg-sky-600 hover:scale-105 transition-all"
+        >
+          <PiPlusCircleBold className="text-2xl mr-2" />
+          Add Category
+        </button>
+      </div>
+
+      <div className="grid grid-cols-4 gap-4">
+        {expenseCategories.map((expense, key) => (
+          <div
+            className="bg-white flex items-center p-3 rounded-lg cursor-pointer"
+            onClick={() => handleClick(expense)}
+            key={key}
+          >
+            <div className="rounded-full bg-sky-300 aspect-square flex items-center justify-center p-2 shadow-lg cursor-pointer hover:bg-sky-400 mr-3">
+              <DinamicIcon style="text-2xl" iconName={expense.icon_name} />
+            </div>
+            <p>{expense.category_name}</p>
           </div>
-          <p>Beauty</p>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+
+      {showModal && (
+        <AddEditIncomeExpense
+          setShowModal={setShowModal}
+          item={expense}
+          category="expense"
+        />
+      )}
+    </>
   );
 }
 
